614ad793fe2f741dc3ffc8f901ba02bf60cfc0ea
Sonali Sinha <91220775+Finbaba@users.noreply.github.com>
2025-04-19 16:22:04 +0530

    Add files via upload

---
+++ app/Http/kernel.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * This middleware will be run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \App\Http\Middleware\SetTenantId::class,  // Your custom middleware for tenant_id
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * These middleware groups may be assigned to groups of routes.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to individual routes.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'tenant' => \App\Http\Middleware\SetTenantId::class,  // Your custom middleware for tenant_id
+    ];
+}
